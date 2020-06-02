a5c86c48030b8c126d7387561e32111497dbccce
Magdalena Painter <paintermagdalena@gmail.com>
2020-06-02 15:21:54 -0400

    done

--- app.js
+++ app.js
@@ -9,29 +9,43 @@ const googlePlayApps = require('./playstore')
 
 
 app.get('/playApps', (req, res) => {
-    const goodSorts = ['Rating','App'];
-    const { type, sort } = req.query;
-    let currentResponse = [ ...googlePlayApps ];
+  const goodSorts = ['Rating', 'App'];
+  const genresFilter = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+  const { genres, sort } = req.query;
+  let currentResponse = [...googlePlayApps];
 
-    if (sort && !goodSorts.includes(sort)) {
-        return res
-            .status(400)
-            .json({ error: 'Sort query must be of "rating" or "app"'});
-    }
+  if (sort && !goodSorts.includes(sort)) {
+    return res
+      .status(400)
+      .json({ error: 'Sort query must be of "rating" or "app"' });
+  }
 
-    if (sort) {
-        currentResponse.sort((a, b) => {
-            if ( a[sort] < b[sort] ) {
-                return -1;
-            } else {
-                return 1;
-            }
-        });
-    }
-    res.json(currentResponse);
+  if (sort) {
+    currentResponse.sort((a, b) => {
+      if (a[sort] < b[sort]) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  }
+
+  if (genres && !genresFilter.includes(genres)) {
+    return res
+      .status(400)
+      .json({ error: 'Not a valid genres' });
+  }
+
+  if (genres) {
+    currentResponse = currentResponse.filter(result =>
+      result.Genres.includes(genres)
+    );
+  }
+  
+  res.json(currentResponse);
 
 });
 
-app.listen(8000,  () => {
-    console.log('Server started on PORT 8000')
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000')
 });
